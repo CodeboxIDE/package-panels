8b433889aa0574ecc4c1c4ded507c97ddc00dbd3
Samy Pessé <samypesse@gmail.com>
2015-04-08 11:37:50 +0200

    Adapt for browserify build

--- src/index.js
+++ src/index.js
@@ -1,20 +1,18 @@
-define([
-    "less!src/stylesheets/main.less"
-], function() {
-    var manager = new codebox.tabs.Manager({
-        tabMenu: false
-    });
-    manager.$el.addClass("component-panels");
+require("./stylesheets/main.less");
 
-    // Add tabs to grid
-    codebox.app.grid.addView(manager, {
-        width: 20,
-        at: 0
-    });
-
-    // Render the tabs manager
-    manager.render();
-
-    // Make the tab manager global
-    codebox.panels = manager;
+var manager = new codebox.tabs.Manager({
+    tabMenu: false
 });
+manager.$el.addClass("component-panels");
+
+// Add tabs to grid
+codebox.app.grid.addView(manager, {
+    width: 20,
+    at: 0
+});
+
+// Render the tabs manager
+manager.render();
+
+// Make the tab manager global
+codebox.panels = manager;
